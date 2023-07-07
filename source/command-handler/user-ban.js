c4cd4c5a7a17dfb385cb1de68ad11b193dca7b6a
git-cameronbryce <developmentcameronbryce@gmail.com>
2023-07-07 12:01:43 -0600

    Added database logic, counter, and logs.

--- source/command-handler/user-ban.js
+++ source/command-handler/user-ban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
 const { FieldValue } = require('@google-cloud/firestore')
 const { db } = require('../script.js')
 const axios = require('axios');
@@ -13,6 +13,8 @@ module.exports = {
     .addStringOption(option => option.setName('reason').setDescription('description').setRequired(true)),
 
   async execute(interaction) {
+    await interaction.deferReply({ ephemeral: false });
+
     const input = {
       username: interaction.options.getString('username'),
       reason: interaction.options.getString('reason'),
@@ -20,24 +22,54 @@ module.exports = {
       admin: interaction.user.id,
     };
 
+    const responseFailure = async ({ username, reason, guild }) => {
+      console.log('Ban failed: ... ... ...')
+    };
+
+    const responseSuccess = async ({ username, reason, guild, admin }) => {
+      const unix = Math.floor(Date.now() / 1000);
+      await db.collection('player-banned').doc(guild).set({
+        [username]: { reason: reason, unix: unix, admin: admin }
+      }, { merge: true })
+    };
+
     const reference = (await db.collection('configuration').doc(input.guild).get()).data();
-    console.log(reference.tokens)
+    console.log(reference.tokens);
 
-    const url = 'https://api.nitrado.net/services'
-    const response = await axios.get(url, { headers: { 'Authorization': reference.tokens[0] } })
+    const url = 'https://api.nitrado.net/services';
+    const response = await axios.get(url, { headers: { 'Authorization': reference.tokens[0] } });
+    const serverArray = [...response.data.data.services]; // Total servers, used for ban calc.
 
-    let failure = 0;
-    let success = 0;
-    const action = response.data.data.services.map(async server => {
-      const url = `https://api.nitrado.net/services/${server.id}/gameservers/games/banlist`;
-      const response = await axios.post(url, { identifier: input.username }, { headers: { 'Authorization': reference.tokens[0] } });
-      console.log(response.status);
 
-      response.status === 200 ? success++ : failure++
-    });
+    try {
+      let failure = 0;
+      let success = 0;
+      const action = response.data.data.services.map(async server => {
+        const url = `https://api.nitrado.net/services/${server.id}/gameservers/games/banlist`;
+        const response = await axios.post(url, { identifier: input.username }, { headers: { 'Authorization': reference.tokens[0] } });
+        console.log(response.status);
 
-    await Promise.all(action);
-    console.log(success, failure)
+        response.status === 200 ? responseSuccess(input, success++) : responseFailure(input, failure++)
+      });
+
+      await Promise.all(action);
+      const metadata = new ButtonBuilder()
+        .setCustomId('metadata')
+        .setLabel('Return Metadata')
+        .setStyle(ButtonStyle.Success);
+
+      const row = new ActionRowBuilder()
+        .addComponents(metadata);
+
+      const embed = new EmbedBuilder()
+        .setColor('#2ecc71')
+        .setTitle('`Obelisk Management`')
+        .setDescription(`\`🟢\` \`System Success\`\nThe selected user has been banned.\nBanned from \`${success}\` of \`${serverArray.length}\` servers.`)
+        .setFooter({ text: 'Tip: Contact support if there are issues.' })
+
+      await interaction.followUp({ embeds: [embed], components: [row] })
+
+    } catch (error) { responseFailure(input) }
 
   }
 };
