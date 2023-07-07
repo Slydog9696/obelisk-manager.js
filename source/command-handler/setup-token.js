bd1bf1e50cc0297a4ca62536fee7932f49b3a07b
git-cameronbryce <developmentcameronbryce@gmail.com>
2023-07-06 21:12:25 -0600

    Added channel creation, if token valid.

--- source/command-handler/setup-token.js
+++ source/command-handler/setup-token.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 const { db } = require('../script.js')
 const axios = require('axios');
 
@@ -14,16 +14,54 @@ module.exports = {
     const input = {
       token: interaction.options.getString('token'),
       guild: interaction.guild.id,
-      admin: interaction.user.id,
+      admin: interaction.user.id
     };
 
     const invalidToken = async ({ token }) => {
       console.log(`Invalid: ${token}`)
     }
 
-    const validToken = async ({ token }) => {
-      const reference = (await db.collection('configuration').doc(input.guild).get()).data();
+    const validToken = async ({ token, guild }) => {
       console.log(`Valid: ${token}`)
+
+      const mainManagement = await interaction.guild.channels.create({
+        name: `Obelisk Management`,
+        type: ChannelType.GuildCategory,
+      });
+
+      const command = await interaction.guild.channels.create({
+        name: '⚫│𝗖ommand-𝗟ogs',
+        type: ChannelType.GuildText,
+        parent: mainManagement,
+      });
+
+      const status = await interaction.guild.channels.create({
+        name: '⚫│𝗦erver-𝗦tatus',
+        type: ChannelType.GuildText,
+        parent: mainManagement,
+      });
+
+      const message = await status.send({ content: 'T' })
+
+      const mainLogging = await interaction.guild.channels.create({
+        name: `Obelisk Protection`,
+        type: ChannelType.GuildCategory,
+      });
+
+      const audit = await interaction.guild.channels.create({
+        name: '🍻│𝗔udit-𝗟ogging',
+        type: ChannelType.GuildText,
+        parent: mainLogging,
+      });
+
+      const reference = await db.collection('configuration').doc(guild)
+        .set(
+          {
+            ['channels']: { status: status.id, message: message.id },
+            ['logging']: { audit: audit.id },
+            ['token']: { token: token }
+          }, { merge: true }
+        );
     }
 
     try {  // Try/Catch for Nitrado issues.
@@ -35,3 +73,5 @@ module.exports = {
 
   }
 };
+
+//       const reference = (await db.collection('configuration').doc(input.guild).get()).data();
