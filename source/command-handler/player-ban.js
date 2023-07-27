d957cb59fa742364dd4a3febba8c61ba960115f7
git-cameronbryce <developmentcameronbryce@gmail.com>
2023-07-27 10:03:51 -0600

    Revisited project structure.

--- source/command-handler/player-ban.js
+++ source/command-handler/player-ban.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { db } = require('../script.js')
+const { db } = require('../script.js');
 const axios = require('axios');
 
 process.on("unhandledRejection", (err) => console.error(err));
@@ -75,29 +75,5 @@ module.exports = {
       .setFooter({ text: 'Tip: Contact support if there are issues.' })
 
     await interaction.followUp({ embeds: [embed] });
-
-    const validPlayer = async ({ name, server, id, last_online }) => {
-      const unix = Math.floor(Date.parse(last_online) / 1000);
-
-      await db.collection('player-metadata').doc('metadata').set({
-        [name]: { server_name: server.details.name, uuid: id, last_online: unix, reason: reason }
-      }, { merge: true })
-    };
-
-    const filterPlayer = async (players, server) => {
-      players.forEach(player => player.name === username ? validPlayer({ ...player, server }) : null)
-    };
-
-    try {
-      response.data.data.services.forEach(async server => {
-        const url = `https://api.nitrado.net/services/${server.id}/gameservers/games/players`;
-        const response = await axios.get(url, { headers: { 'Authorization': reference.tokens[0] } });
-        response.status === 200 ? filterPlayer(response.data.data.players, server) : null
-      });
-
-    } catch (error) { console.log('Cannot locate in-game player.') };
   }
 };
-
-//? Ban failed, search player, collect uuid. 
-//? Search player failed, search database. 
