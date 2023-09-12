bae5d9a700a38ad18a3277cad9ef52ccefca9c73
git-cameronbryce <developmentcameronbryce@gmail.com>
2023-09-12 00:11:24 -0600

    Finished metadata collector.

--- source/event-handler/event-metadata.js
+++ source/event-handler/event-metadata.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, Embed, EmbedBuilder } = require('discord.js');
 const { db } = require('../script');
 const axios = require('axios');
 
@@ -8,12 +8,10 @@ module.exports = {
   execute(client) {
     async function loop() {
 
-      //! Collects the players, and the server name, loops through each player.
-      //! If player does not exist, pushes player information to database.
       const playerCollection = async (players, { details }) => {
         const reference = (await db.collection('player-metadata').doc('metadata').get()).data()
         players.forEach(async player => {
-          const { id, name, last_online } = player;
+          const { id, name, last_online, online } = player;
           reference[name] ? null : await db.collection('player-metadata').doc('metadata')
             .set({ [name]: { name: name, uuid: id, last_online, server: details.name } }, { merge: true }).then(() => {
               console.log(`Database Written: ${name}`)
@@ -21,7 +19,6 @@ module.exports = {
         })
       }
 
-      //! Loops through each server, if the response is successful, runs: playerCollection()
       const validDocument = async ({ nitrado }) => {
         const url = 'https://api.nitrado.net/services';
         const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
@@ -33,14 +30,12 @@ module.exports = {
         })
       }
 
-      //! Loops through each document, if the document exists, runs: validDocument()
-      //! Passes doc.data() which holds the token: 'Nitrado'
       const reference = await db.collection('configuration').get();
       reference.forEach(doc => {
         doc.data() ? validDocument(doc.data()) : console.log('Invalid document.');
       });
       setTimeout(loop, 60000);
     }
-    loop().then(() => console.log('Loop started:'));
+    // loop().then(() => console.log('Loop started:'));
   },
 };
